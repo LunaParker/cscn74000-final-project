c173f423ce90427b95bf3ac1e85132e7f6b613b7
Dante <dpalalasmouradi8676@conestogac.on.ca>
2026-03-26 14:09:47 -0400

    First iteration of PacketHeader and Telemetry Payload

--- src/common/packets.h
+++ src/common/packets.h
@@ -1,4 +1,28 @@
 #ifndef CSCN74000_PROJECT_PACKETS_H
 #define CSCN74000_PROJECT_PACKETS_H
 
+#include <cstdint>
+
+/**
+ * @brief Standard Packet Header
+ */
+struct PacketHeader
+{
+    int32_t aircraftId;
+    uint32_t payloadSize;
+};
+
+/**
+ * @brief Telemetry payload containing timestamped spatial coordinates.
+ */
+struct TelemetryPayload
+{
+    int64_t timestamp;
+    double latitude;
+    double longitude;
+    double altitude;
+
+};
 #endif
+
+
