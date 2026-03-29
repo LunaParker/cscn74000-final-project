47b77350299a718395b7bedc000cb1012e53bb9a
Dante <dpalalasmouradi8676@conestogac.on.ca>
2026-03-29 12:21:55 -0400

    Logger Header prototype created

--- src/common/logging.h
+++ src/common/logging.h
@@ -1,4 +1,28 @@
 #ifndef CSCN74000_PROJECT_LOGGING_H
 #define CSCN74000_PROJECT_LOGGING_H
 
+#include <string>
+#include <fstream>
+
+class Logger {
+public:
+    Logger();
+    ~Logger();
+
+    //Deleted copy constructor and assignment.
+    Logger(const Logger&) = delete;
+    Logger& operator=(const Logger&) = delete;
+
+    /**
+     * @brief Logs a communication event.
+     * @param sender The ID or name of the sender.
+     * @param direction "INCOMING" or "OUTGOING".
+     * @param message The content or packet type description.
+     */
+    void logEvent(const std::string& sender, const std::string& direction, const std::string& message);
+
+    private:
+        std::ofstream logFile;
+        void openDailyFile(); // Creates file using the current date
+};
 #endif
