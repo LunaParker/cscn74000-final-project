324c3be828ac31cf5c3d7541c3622fdf104916d0
Jon Ward <jcmward@gmail.com>
2026-03-31 15:01:36 -0400

    fix: generate log filename with UTC time
    Use time in addition to date for log filename, to prevent overwriting a
    log file.  Create a new log file on every system boot.  Use UTC time in
    log filename and log entries, to avoid ambiguity.
    
    See #42

--- src/common/logging.cpp
+++ src/common/logging.cpp
@@ -19,10 +19,9 @@ void Logger::openDailyFile() {
     auto in_time_t = std::chrono::system_clock::to_time_t(now);
 
     std::stringstream ss;
-    // Format: YYYY-MM-DD_log.txt for traceability
-    ss << std::put_time(std::localtime(&in_time_t), "%Y-%m-%d") << "_log.txt";
-
-    logFile.open(ss.str(), std::ios::app);
+    // Log filename format: YYYY-MM-DD_HH-MM-SS_log.txt
+    ss << std::put_time(std::gmtime(&in_time_t), "%Y-%m-%d_%H-%M-%S") << "_UTC_log.txt";
+    logFile.open(ss.str(), std::ios::out);
 }
 
 void Logger::logEvent(const std::string &sender, const std::string &direction, const std::string &message) {
@@ -31,7 +30,7 @@ void Logger::logEvent(const std::string &sender, const std::string &direction, c
         auto in_time_t = std::chrono::system_clock::to_time_t(now);
 
         // Required fields for logging: Timestamp, Direction, Sender
-        logFile << "[" << std::put_time(std::localtime(&in_time_t), "%Y-%m-%d %H:%M:%S") << "] "
+        logFile << "[" << std::put_time(std::gmtime(&in_time_t), "%Y-%m-%d %H:%M:%S UTC") << "] "
                 << "Dir: " << direction << " | "
                 << "From: " << sender << " | "
                 << "Data: " << message << '\n';
